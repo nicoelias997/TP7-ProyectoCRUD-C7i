1a6dd7830953c66c5667dc09830b1a61a3490d20
nicoelias997 <nicoelias997@gmail.com>
2022-08-22 20:21:24 +0200

    Agregamos la classPelicula para crear distintas pelis, y agregamos la funcion agregarPelicula y limpiarFormulario

---
+++ js/admin.js
@@ -0,0 +1,47 @@
+import { Pelicula } from "./classPelicula.js";
+
+let listaPeliculas = []; //aqui voy a guardar todas las peliculas
+
+//traer los input
+let codigo = document.querySelector('#codigo');
+let titulo = document.querySelector('#titulo');
+let descripcion = document.querySelector('#descripcion');
+let imagen = document.querySelector('#imagen');
+let genero = document.querySelector('#genero');
+let formPelicula = document.querySelector('#formPelicula');
+let btnCrearPelicula = document.querySelector('#btnCrearPelicula');
+// crear una instancia de la ventana modal
+const modalAdminPelicula = new bootstrap.Modal(document.querySelector('#modalPelicula'));
+
+//agregar el evento
+
+btnCrearPelicula.addEventListener('click', crearPelicula);
+formPelicula.addEventListener('submit', guardarPelicula);
+
+function crearPelicula(){
+    //mostrar ventana modal
+    modalAdminPelicula.show();
+    //generar el identificador unico y asignarlo al campo del codigo
+    codigo.value = uuidv4();
+    // console.log( uuidv4()); esta libreria genera identificadores unicos
+}
+
+function guardarPelicula(e){
+    e.preventDefault();
+   //volver a validar todos los campos
+
+   //si los datos son correctos
+   let nuevaPelicula =  new Pelicula(codigo.value, titulo.value, descripcion.value, imagen.value, genero.value);
+   console.log(nuevaPelicula)
+   listaPeliculas.push(nuevaPelicula);
+   //limpiar formulario
+   limpiarFormulario();
+   console.log(listaPeliculas);
+   //cerrar la ventana modal
+   modalAdminPelicula.hide();
+}
+
+function limpiarFormulario(){
+    formPelicula.reset()
+    // modificar las clases de bootstrap si es necesario
+}
---
+++ js/classPelicula.js
@@ -0,0 +1,9 @@
+export class Pelicula {
+    constructor(codigo, titulo, descripcion, image, genero){
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.image = image;
+        this.genero = genero;
+    }
+}
